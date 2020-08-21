c01b46727b44b49249d71bdc7a843b63fbd5e431
manfye <manfye@gmail.com>
2020-08-22 01:00:43 +0800

    post event

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const bodyParser = require("body-parser");
 const {randomBytes} = require("crypto")
 const cors = require("cors")
+const axios = require("axios")
 
 const app = express()
 
@@ -14,7 +15,7 @@ app.get('/posts/:id/comments', (req, res)=>{
     res.send(commentsByPostID[req.params.id]|| []);
 });
 
-app.post('/posts/:id/comments', (req, res)=>{
+app.post('/posts/:id/comments', async (req, res)=>{
     const commentID = randomBytes(4).toString("hex");
     const {content}= req.body
     const comments = commentsByPostID[req.params.id] || []
@@ -22,6 +23,15 @@ app.post('/posts/:id/comments', (req, res)=>{
     comments.push({id:commentID, content})
 
     commentsByPostID[req.params.id] = comments
+
+    await axios.post("http://localhost:4005/events",{
+        type: "CommentCreated",
+        data:{
+            id:commentID, content,
+        postId: req.params.id
+        
+        }
+      })
 
     res.status(201).send(comments)
 
